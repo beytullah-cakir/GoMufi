99fdebf76fe6a188484749304a6dae681333a372
EsraÇimen <eesra8352@gmail.com>
2026-04-08 20:01:51 +0300

    generate questions system has been added

--- backend/main_fastapi.py
+++ backend/main_fastapi.py
@@ -6,7 +6,14 @@ from connect_db import SessionLocal, engine, Base
 from starlette.middleware.sessions import SessionMiddleware
 from routers import profile, courses, student_auth, teacher_auth, oauth, builder, payment
 import os
-from models import Student, Teacher, Course, Enrollment
+from models import Student, Teacher, Course, Enrollment # quiz ekledim
+from models.Quiz import Quiz
+
+#yz için 
+from fastapi import Request, HTTPException, Depends # <-- Bunları ekle
+from sqlalchemy.orm import Session 
+import quiz_service
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -15,6 +22,13 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 # 1. PROXY HEADERS (Railway için zorunlu)
 app.add_middleware(ProxyHeadersMiddleware, trusted_hosts="*")
@@ -49,6 +63,48 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+@app.post("/generate_quiz")
+async def generate_quiz(request: Request, db: Session = Depends(get_db)):
+    data = await request.json()
+    topic = data.get('topic')
+    difficulty = data.get('difficult', 'Orta')
+
+    if not topic:
+        raise HTTPException(status_code=400, detail='Lütfen "topic" parametresi gönderin')
+
+    result = quiz_service.generate_quiz_question(topic, difficulty)
+
+    if result.get('success'):
+        try:
+            new_quiz = Quiz(
+                topic=topic,
+                difficulty=difficulty,
+                question_text=result['quiz']['soru'],
+                options=result['quiz']['secenekler'],
+                correct_answer=result['quiz']['cevap']
+            )
+            db.add(new_quiz)
+            db.commit()
+            db.refresh(new_quiz)
+            
+            result['db_id'] = new_quiz.id
+            return result
+        except Exception as e:
+            db.rollback()
+            raise HTTPException(status_code=500, detail=f"Veritabanı kayıt hatası: {str(e)}")
+    else:
+        raise HTTPException(status_code=500, detail="Quiz üretilemedi")
+
+@app.get("/quizzes")
+async def get_quizzes(db: Session = Depends(get_db)):
+    quizzes = db.query(Quiz).order_by(Quiz.id.desc()).all()
+    return {
+        "success": True,
+        "count": len(quizzes),
+        "data": [q.to_dict() for q in quizzes]
+    }
 
 app.include_router(student_auth.router)
 app.include_router(teacher_auth.router)
--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -5,3 +5,4 @@ from .teacher import Teacher
 from .tag import Tag
 from .parent import Parent
 from .live_session import LiveSession
+from .Quiz import Quiz  
